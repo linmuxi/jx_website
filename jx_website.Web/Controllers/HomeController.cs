5019b35fa85e53ad7b56d430dea4a3d56823f31c
linmuxi <2010_lin@sina.cn>
2016-04-06 10:23:53 +0800

    update

--- jx_website.Web/Controllers/HomeController.cs
+++ jx_website.Web/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using jx_website.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -35,7 +38,28 @@ namespace jx_website.Web.Controllers
         // 显示借款信息
         public JsonResult BorrowInfo()
         {
-            return Json(new Message(true, "湖北武汉王先生借款50000元，用于购买65145G（13：30）"+DateTime.Now));
+            string paramstr = "{\"pass\":\"jxjr_12346app\",\"date\":\"\"}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://218.17.125.60:6150/api/Android/DisplayData");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(paramstr);
+            // request.CookieContainer = cookie;
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(paramstr);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // response.Cookies = cookie.GetCookies(response.ResponseUri);
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            //return Json(new Message(true, "湖北武汉王先生借款50000元，用于购买65145G（13：30）"+DateTime.Now));
+            return Json(new Message(true, retString));
         }
 
     }
